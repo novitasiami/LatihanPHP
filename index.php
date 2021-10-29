5e291d97384a89fa1e3e4880e354f4bf1564cadb
Mailanfms01 <93360371+Mailanfms01@users.noreply.github.com>
2021-10-29 21:50:04 +0700

    Update Index

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo"Selamat Datang";
+echo"<h2>Tugas Kelompok Pemrograman Internet</h2>";
+echo"Anggota :<br>";
+echo"Deta Shafa Fadhilah<br>";
+echo"Mailan Fitri Mar'atu Solihah<br>";
+echo"Novita Siami<br>";
+echo"Rahma Lestari<br>";
+echo"Sofia Rahmah S<br>";
+
 ?>
